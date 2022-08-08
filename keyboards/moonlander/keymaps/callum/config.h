328cd6a652ab269c703a3c7d2033f4c6940912ec
Kamal Arieff <kamal.arieff@mudah.my>
2022-08-08 17:53:53 +0800

    [callum] working keymap

--- keyboards/moonlander/keymaps/callum/config.h
+++ keyboards/moonlander/keymaps/callum/config.h
@@ -4,7 +4,7 @@
 */
 #define ORYX_CONFIGURATOR
 #undef TAPPING_TERM
-#define TAPPING_TERM 160
+#define TAPPING_TERM 200
 
 // https://www.reddit.com/r/olkb/comments/qu7vd8/taphold_settings_for_fast_typers/
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -70,7 +70,7 @@
 #define FIRMWARE_VERSION u8"7B4by/mJeJm"
 #define RGB_MATRIX_STARTUP_SPD 60
 
-#define COMBO_COUNT 8
+#define COMBO_COUNT 1
 #define COMBO_TERM 30
 
 #undef LEADER_NO_TIMEOUT
